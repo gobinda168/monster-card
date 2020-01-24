cf6d7b7e3b255e5a01ed3190817679eff9046482
Gobinda Das <gobinda168@gmail.com>
2020-01-24 19:52:13 +0530

    Updates

---
+++ precache-manifest.665cea5c39317d0b08f2f75a3e6d573e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c9988cdf483e9e0a5fe631d2dc5c84",
+    "url": "/monster-card/index.html"
+  },
+  {
+    "revision": "5e287acc799a996802c8",
+    "url": "/monster-card/static/css/main.f581e242.chunk.css"
+  },
+  {
+    "revision": "00c4bd6350d25d78e76c",
+    "url": "/monster-card/static/js/2.29b3eaca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-card/static/js/2.29b3eaca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e287acc799a996802c8",
+    "url": "/monster-card/static/js/main.77c10584.chunk.js"
+  },
+  {
+    "revision": "27cf31498f968368c74a",
+    "url": "/monster-card/static/js/runtime-main.b3d38bc4.js"
+  }
+]);
